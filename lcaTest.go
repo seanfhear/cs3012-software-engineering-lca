dbd1de053fce5287f619363ed080086ef7d28992
Sean-Hassett <hassetse@tcd.ie>
2018-09-23 12:42:08 +0100

    added function names for some test cases

--- lcaTest.go
+++ lcaTest.go
@@ -2,6 +2,27 @@ package main
 
 import "testing"
 
-func TestSomething(t *testing.T) {
+// Test a valid LCA is returned
+func TestValidLCA(t *testing.T) {
+
 }
 
+// Test when one of the nodes is the LCA
+func TestOneNodeIsLCA(t *testing.T) {
+
+}
+
+// Test when both given nodes are the same
+func TestDuplicateNode(t *testing.T) {
+
+}
+
+// Test when wrong amount of nodes given
+func TestIncorrectNumParameters(t *testing.T) {
+
+}
+
+// Test invalid parameters
+func TestInvalidParameters(t *testing.T) {
+
+}
