d78e1013d3bd60db481e1d7901cfb98162fd8242
Sean-Hassett <hassetse@tcd.ie>
2018-11-02 11:04:31 +0000

    fixed bug with nodes remaining seen after getDistanceVector

--- lca/lca.go
+++ lca/lca.go
@@ -3,21 +3,30 @@ package lca
 import "fmt"
 
 // finds lowest common ancestor of two given nodes and returns the LCA as a TreeNode
-func LowestCommonAncestor(p, q *Node) *Node {
+func LowestCommonAncestor(g *Graph, p, q *Node) *Node {
 	pVector := make(map[int][]*Node)
-	getDistanceVector(0, p, pVector)
+	qVector := make(map[int][]*Node)
+	getDistanceVectorInit(g, p, pVector)
+	getDistanceVectorInit(g, q, qVector)
 
 	fmt.Println(pVector)
-
+	fmt.Println(qVector)
 
 	return nil
 }
 
-// breadth-first search through the graph from the source node to all ancestors
+// helper function to reset all nodes back to unseen after GDV
+func getDistanceVectorInit(g *Graph, a *Node, aVector map[int][]*Node) {
+	getDistanceVector(0, a, aVector)
+	for _, node := range g.Nodes {
+		node.Seen = false
+	}
+}
+
+// depth-first search through the graph from the source node to all ancestors
 // recording distance of each ancestor from the source
 func getDistanceVector(distance int, a *Node, aVector map[int][]*Node) {
 	a.Seen = true
-
 	for _, p := range a.Parents {
 		if !p.Seen {
 			distance += 1
